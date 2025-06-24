99c9f8124dcae3ac14e4accce50d877cb7e3bc82
dlskl <skyandme77@gmail.com>
2025-06-24 15:08:02 +0900

    fix : fix header menu
    무디모드-헤더 메뉴 hover 추가 및 style 변경

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,9 +6,11 @@ const Header = () => {
     <div className="header-container">
       <span className="logo-text">무디모드</span>
       <div>
-        <Link id="menu">감정 통계</Link>
-        <Link id="menu">일기 보기</Link>
-        <Link id="menu">일기 쓰기</Link>
+        <Link className="menu">감정 통계</Link>
+        <Link className="menu">일기 보기</Link>
+        <Link className="menu" to="/main">
+          일기 쓰기
+        </Link>
       </div>
     </div>
   );
