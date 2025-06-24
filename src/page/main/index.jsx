e19da017f40bf06446ed8e5d70740d84499117d1
dlskl <skyandme77@gmail.com>
2025-06-24 15:15:20 +0900

    fix : fix help word and style
    무디모드-도움말 추가 및 스타일 수정

--- src/page/main/index.jsx
+++ src/page/main/index.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/header";
-import Write from "../../components/write"
-import "./style.css"
+import Write from "../../components/write";
+import "./style.css";
 
 const MainPage = () => {
   const today = new Date();
@@ -11,14 +11,15 @@ const MainPage = () => {
 
   return (
     <>
-    <div style={{ textAlign: "center" }}>
-      <Header />
-      <div className="today-text">오늘의 날짜는</div>
-      <div className="today-text">{formattedDate}입니다</div>
+      <div style={{ textAlign: "center" }}>
+        <Header />
+        <div className="today-text">오늘의 날짜는</div>
+        <div className="today-text">{formattedDate}입니다</div>
 
-      <div className="today-feeling">오늘의 기분은 어떠셨나요?</div>
-    </div>
-    <Write/>
+        <div className="today-feeling">오늘의 기분은 어떠셨나요?</div>
+        <p className="help-word">오늘의 감정에 맞는 캐릭터를 선택하고, 일기를 작성해주세요</p>
+      </div>
+      <Write />
     </>
   );
 };
